0bab305d553fbc0c7bd38bb6ab913cb3e146972d
Parsiya Keshavarz <ashalogic@outlook.com>
2023-10-10 03:05:34 +0200

    fix1

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -5,22 +5,4 @@ function main(args) {
   return { body: greeting };
 }
 
-function getUserProfile() {
-  var requestOptions = {
-    method: "GET",
-    redirect: "follow",
-  };
-
-  var data = {};
-  fetch("https://www.instagram.com/taylorswift/?__a=1&__d=dis", requestOptions)
-    .then((response) => {
-      data = response.text();
-    })
-    .then((result) => console.log(result))
-    .catch((error) => console.log("error", error));
-
-  return data;
-}
-
 exports.main = main;
-exports.getUserProfile = getUserProfile;
---
+++ packages/sample/hello/user.js
@@ -0,0 +1,17 @@
+function getUserProfile() {
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  var data = {};
+  fetch("https://www.instagram.com/taylorswift/?__a=1&__d=dis", requestOptions)
+    .then((response) => {
+      data = response.text();
+    })
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+
+  return data;
+}
+exports.getUserProfile = getUserProfile;

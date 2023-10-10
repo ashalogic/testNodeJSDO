5b8406acda8fa065b7e07a6de985a5c82ac56249
Parsiya Keshavarz <ashalogic@outlook.com>
2023-10-10 02:56:18 +0200

    init commit

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,8 +1,26 @@
 function main(args) {
-    let name = args.name || 'stranger'
-    let greeting = 'Hello ' + name + '!'
-    console.log(greeting)
-    return {"body": greeting}
-  }
+  let name = args.name || "stranger";
+  let greeting = "Hello " + name + "!";
+  console.log(greeting);
+  return { body: greeting };
+}
 
-exports.main = main
+function getUserProfile() {
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  var data = {};
+  fetch("https://www.instagram.com/taylorswift/?__a=1&__d=dis", requestOptions)
+    .then((response) => {
+      data = response.text();
+    })
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+
+  return data;
+}
+
+exports.main = main;
+exports.getUserProfile = getUserProfile;
